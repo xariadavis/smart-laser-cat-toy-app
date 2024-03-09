839c69de919a451e6445acb3cbcd238097e8f322
xariadavis <xariadavis@knights.ucf.edu>
2024-03-08 23:30:45 -0500

    initial commit of v4 T-T

---
+++ SD App V4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SD App V4
+//
+//  Created by Xaria Davis on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SD App V4/SD_App_V4App.swift
@@ -0,0 +1,17 @@
+//
+//  SD_App_V4App.swift
+//  SD App V4
+//
+//  Created by Xaria Davis on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SD_App_V4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
